d34c5c758759dbfa90a33639399d44c24fead76a
Artem <ASaprykin97@gmail.com>
2017-03-04 16:07:00 +0200

    task 1b done

--- lab1/main/main.py
+++ lab1/main/main.py
@@ -1,6 +1,10 @@
 import re
 
 if __name__ == '__main__':
+    print('task 1a')
     reg = re.compile('(\[(\+|-)([0-9]+|[A-Z]+)\])')
     for r in re.findall(reg, open('1.txt','r').read()):
         print(r[0])
+    print('task 1b')
+    for m in reg.finditer(open('2.txt', 'r').read()):
+        print(m.start(), m.group())
