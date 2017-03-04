0d9bc261ad2012760f81b7a1bbb4b929b9efb46e
Artem <ASaprykin97@gmail.com>
2017-03-04 15:53:00 +0200

    1a done

--- lab1/main/main.py
+++ lab1/main/main.py
@@ -1,4 +1,6 @@
-
+import re
 
 if __name__ == '__main__':
-    print('hello world')
+    reg = re.compile('(\[(\+|-)([0-9]+|[A-Z]+)\])')
+    for r in re.findall(reg, open('1.txt','r').read()):
+        print(r[0])
